1a0bc04963f2e7a57e3462bb457f2151fd14f020
AndrewSmirnovWork <andrew.smirnov.work@gmail.com>
2020-03-17 19:42:26 +0300

    trying to test

--- DirectoryOfRegions/src/main/java/com/werdna/DirectoryOfRegions/dao/RegionDAOImpl.java
+++ DirectoryOfRegions/src/main/java/com/werdna/DirectoryOfRegions/dao/RegionDAOImpl.java
@@ -1,36 +1,5 @@
 package com.werdna.DirectoryOfRegions.dao;
 
-import com.werdna.DirectoryOfRegions.entity.Region;
-import org.apache.ibatis.annotations.*;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-
-public class RegionDAOImpl {/*
-
-
-    @Override
-    @Select("Select * from directory")
-    public List<Region> findAll() {
-        return null;
-    }
-
-    @Override
-    @Select(
-            "Select id, fullName, shortName from directory where id=#{id}")
-    @Results(value = {
-            @Result(property = "id", column = "id"),
-            @Result(property="fullName", column = "fullName"),
-            @Result(property = "shortName", column = "shortName")
-    })
-    public Region findById(Integer id) {
-        return null;
-    }
-
-    @Override
-    @Update("Update directory set fullName = #{fullName}, shortName = #{shortName} where id=#{id}")
-    public void update(Region region) {
-
-    }*/
+public class RegionDAOImpl {
 }
